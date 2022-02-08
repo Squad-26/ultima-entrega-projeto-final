e3b4a2b30ff02753bfba612d8dc07aa520008f18
JonasJesus <jonasdasilvajesus@outlook.com>
2022-02-08 09:28:57 -0300

    d

--- frontend/app-acecidade-v2/src/page/locais/Locais.js
+++ frontend/app-acecidade-v2/src/page/locais/Locais.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Box } from '@skynexui/components';
 
 export default function Locais(){
     return(
-        <>
-            locais
-        </>
+<Box
+  styleSheet={{
+    backgroundColor: {
+      xl: '#222'
+    },
+    color: 'orange',
+    height: '100vw',
+    padding: '16px'
+  }}
+>
+  Locais
+</Box>
     )
 }
