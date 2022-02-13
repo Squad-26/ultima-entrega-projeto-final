756f76f4ab372637c5ce2123a222bb42cc06bc90
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-02-13 20:18:24 -0300

    inicio da locais

--- frontend/app-acecidade-v2/src/page/locais/Locais.js
+++ frontend/app-acecidade-v2/src/page/locais/Locais.js
@@ -2,54 +2,89 @@ import React from 'react'
 import { Box, Image } from '@skynexui/components'
 export default function Locais() {
   return (
+  <Box styleSheet={{
+    display:'flex'
+  }}>
     <Box
-      tag="div"
-      styleSheet={{
-        backgroundColor: { xl: '#222' },
-        color: 'orange',
-        padding: '10px',
-        height: '100vh',
-        'flex-wrap': 'wrap',
-        'justify-content': 'space-between'
-      }}
-    >
-      <div>Pizzaria Street Food Art</div>
-      <Box
-        styleSheet={{
-          backgroundColor: {
-            xl: 'orange'
-          },
-          color: 'white',
-          margin: '16px',
-          padding: '16px'
-        }}
-      >
-        <Image
-          src="https://images.vexels.com/media/users/3/128437/isolated/lists/2dd809b7c15968cb7cc577b2cb49c84f-logotipo-de-restaurante-de-comida-de-pizza.png"
-          styleSheet={{
-            border: ''
-          }}
-        />
-      </Box>
+  styleSheet={{
+    backgroundColor: 'black',
+    color: 'white',
+    height: '100vh',
+    margin: '35px 10px',
+    padding: '16px',
+    width: '25vw',
+    borderRadius:'10px'
+  }}
+>
+  Box Component
+</Box>
+  <Box
+    styleSheet={{
+      backgroundColor: 'black',
+      color: 'white',
+      height: 'auto',
+      margin: '35px 10px',
+      padding: '16px',
+      width: '70vw',
+      borderRadius:'10px'
+    }}
+  >
 
-      <div>Restaurante Toca da Cancela</div>
-      <Box
-        styleSheet={{
-          backgroundColor: {
-            xl: 'orange'
-          },
-          color: 'white',
-          margin: '16px',
-          padding: '16px'
-        }}
-      >
-        <Image
-          src="https://tocadacancela.pedzap.com.br/uploads/modules/estabelecimentos/1576_webapp_logomarca_256.png"
-          styleSheet={{
-            border: ''
-          }}
-        />
-      </Box>
-    </Box>
+      <CardRestaurantes nomeRestaurante='bobs' />
+      <CardRestaurantes nomeRestaurante='arroz' />
+      <CardRestaurantes nomeRestaurante='livro' />
+      <CardRestaurantes nomeRestaurante='bobs' />
+      <CardRestaurantes nomeRestaurante='bobs' />
+      <CardRestaurantes nomeRestaurante='bobs' />
+      <CardRestaurantes nomeRestaurante='bobs' />
+      <CardRestaurantes nomeRestaurante='bobs' />
+      <CardRestaurantes nomeRestaurante='bobs' />
+     
+    
+
+  </Box>
+  </Box>
   )
 }
+
+
+function CardRestaurantes({nomeRestaurante='trocar nome', imgSRC="https://images.vexels.com/media/users/3/128437/isolated/lists/2dd809b7c15968cb7cc577b2cb49c84f-logotipo-de-restaurante-de-comida-de-pizza.png"}){
+  return(
+    <>
+    
+    <Box
+        styleSheet={{
+          backgroundColor: {
+            xl: 'orange'
+          },
+          color: 'white',
+          margin: '16px',
+          padding: '16px',
+          display: 'flex'
+        }}
+      >
+        <Image
+          src={imgSRC}
+          styleSheet={{
+            border: ''
+          }}
+          />
+          <div>{nomeRestaurante}</div>
+      </Box>
+    
+    </>
+  )
+}
+
+
+
+/* nessesario em cada req   <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script> */
+
+/* import {GoogleApiWrapper, Map, Maker} from 'google-maps-react'
+
+export const MapContainer = (props) => {}
+
+export default GoogleApiWrapper({
+  apiKay:process.env.GOOGLE_KAY_API,
+  lenguage: 'pt-br'
+})(MapContainer) */
