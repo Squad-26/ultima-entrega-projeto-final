0dda8e559227f66b01dfce2a4ed75e37071093ac
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-02-13 17:53:12 -0300

    home v1

--- frontend/app-acecidade-v2/src/page/home/Home.js
+++ frontend/app-acecidade-v2/src/page/home/Home.js
@@ -1,171 +1,105 @@
 import React from "react";
-import { Box } from '@skynexui/components';
+import { Box, Text, Image } from '@skynexui/components';
 import Colors from "../theme/Colors";
+import './home.style.css'
+import fundo1 from '../../assets/fundo1.jpg'
+import img1 from '../../assets/img1.jpg'
+import img2 from '../../assets/img2.jpg'
 
-export default function Home(){
-    return(
-<Box
-  tag="div"
-  styleSheet={{
-    backgroundColor: {
-      xl: '#222'
-    },
-    color: 'orange',
-    height: '100%',
-    width: '97.7%',
-    padding: '5px',
-    display: 'flex',
-    'max-width': "auto",
-    'flex-wrap': 'wrap',
 
-  }
-  }
->
-    <Box
-      styleSheet={{
-        backgroundColor: {
-          lg: 'purple',
-          md: 'green',
-          sm: 'blue',
-          xl: Colors.Deep_Orange_800,
-          xs: 'red'
-        },
-        color: 'white',
-        height: '60vh',
-        margin: '16px',
-        padding: '16px',
-        width: '300px'
-      }}
-    >
-      asdpajsdfpaskdnfpasdknf 
-    </Box>
-    <Box
-      styleSheet={{
-        backgroundColor: {
-          lg: 'purple',
-          md: 'green',
-          sm: 'blue',
-          xl: Colors.Deep_Orange_800,
-          xs: 'red'
-        },
-        color: 'white',
-        height: '60vh',
-        margin: '16px',
-        padding: '16px',
-        width: '300px'
-      }}
-    >
-      asdpajsdfpaskdnfpasdknf 
-    </Box>
-    <Box
-      styleSheet={{
-        backgroundColor: {
-          lg: 'purple',
-          md: 'green',
-          sm: 'blue',
-          xl: Colors.Deep_Orange_800,
-          xs: 'red'
-        },
-        color: 'white',
-        height: '60vh',
-        margin: '16px',
-        padding: '16px',
-        width: '300px'
-      }}
-    >
-      asdpajsdfpaskdnfpasdknf 
-    </Box>
-    <Box
-      styleSheet={{
-        backgroundColor: {
-          lg: 'purple',
-          md: 'green',
-          sm: 'blue',
-          xl: Colors.Deep_Orange_800,
-          xs: 'red'
-        },
-        color: 'white',
-        height: '60vh',
-        margin: '16px',
-        padding: '16px',
-        width: '300px'
-      }}
-    >
-      asdpajsdfpaskdnfpasdknf 
-    </Box>
-    <Box
-      styleSheet={{
-        backgroundColor: {
-          lg: 'purple',
-          md: 'green',
-          sm: 'blue',
-          xl: Colors.Deep_Orange_800,
-          xs: 'red'
-        },
-        color: 'white',
-        height: '60vh',
-        margin: '16px',
-        padding: '16px',
-        width: '300px'
-      }}
-    >
-      asdpajsdfpaskdnfpasdknf 
-    </Box>
-    <Box
-      styleSheet={{
-        backgroundColor: {
-          lg: 'purple',
-          md: 'green',
-          sm: 'blue',
-          xl: Colors.Deep_Orange_800,
-          xs: 'red'
-        },
-        color: 'white',
-        height: '60vh',
-        margin: '16px',
-        padding: '16px',
-        width: '300px'
-      }}
-    >
-      asdpajsdfpaskdnfpasdknf 
-    </Box>
-    <Box
-      styleSheet={{
-        backgroundColor: {
-          lg: 'purple',
-          md: 'green',
-          sm: 'blue',
-          xl: Colors.Deep_Orange_800,
-          xs: 'red'
-        },
-        color: 'white',
-        height: '60vh',
-        margin: '16px',
-        padding: '16px',
-        width: '300px'
-      }}
-    >
-      asdpajsdfpaskdnfpasdknf 
-    </Box>
-    <Box
-      styleSheet={{
-        backgroundColor: {
-          lg: 'purple',
-          md: 'green',
-          sm: 'blue',
-          xl: Colors.Deep_Orange_800,
-          xs: 'red'
-        },
-        color: 'white',
-        height: '60vh',
-        margin: '16px',
-        padding: '16px',
-        width: '300px'
-      }}
-    >
-      asdpajsdfpaskdnfpasdknf 
-    </Box>
+export default function Home() {
+  return (
+    <>
+      <Box
+        tag="div"
+        styleSheet={{
+          backgroundColor: "white",
+          backgroundImage: `url(${fundo1})`,
+          display: 'flex', alignItems: 'center', justifyContent: 'center', textAlign: 'center',
+          backgroundRepeat: 'no-repeat', backgroundSize: 'cover',
+          color: Colors.Blue_A700,
+          height: '100vh',
+          width: '100vw',
+          padding: '5px',
+          fontSize: '5rem',
+          fontFamily: 'roboto',
+        }}
+      >
+        <h1 className="texto-azul texto-sombra">Na luta por cidades mais<br /><span className="texto-laranja texto-sombra">ACESSÍVEIS</span></h1>
+      </Box>
+      <Box
+        tag="div"
+        styleSheet={{
+          backgroundColor: "black",
+          color: 'orange',
+          height: '100vh',
+          width: '100vw',
+          display: 'flex', alignItems: 'center', justifyContent: 'center',
+        }}
+      >
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/sQGsKXFIg5k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-</Box>
-    )
+      </Box>
+      <Box
+        tag="div"
+        styleSheet={{
+          backgroundColor: "white",
+          color: 'orange',
+          height: '100vh',
+          width: '100vw',
+          display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap',
+        }}
+      >
+        <Box>
+          <Image src={img1}
+            styleSheet={{
+              width: '30rem'
+            }}
+          />
+        </Box>
+        <Text
+          tag="p"
+          variant="h4"
+          className="texto-azul"
+          styleSheet={{
+            width: '600px',
+            fontFamily: 'roboto',
+          }}
+        >
+          Encontre locais que contenham acessibilidade. Encontre locais com rampas de acesso, cardápio em braile, intérprete de libras e outros tipos de acessibilidade, olhe os comentários dos usuários sobre os locais e encontre o local mais confortável para você.
+        </Text>
+
+      </Box>
+      <Box
+        tag="div"
+        styleSheet={{
+          backgroundColor: "black",
+          color: 'orange',
+          height: '100vh',
+          width: '100vw',
+          display: 'flex', alignItems: 'center', justifyContent: 'center',
+        }}
+      >
+        <Text
+          tag="p"
+          variant="h4"
+          className="texto-laranja"
+          styleSheet={{
+            width: '600px',
+            fontFamily: 'roboto',
+          }}
+        >
+          Encontre locais que contenham acessibilidade locais com rampas de acesso, cardápio em braile, intérprete de libras e outros tipos de acessibilidade, olhe os comentários dos usuários sobre os locais e encontre o local mais confortável para você.
+        </Text>
+        <Box>
+          <Image src={img2}
+            styleSheet={{
+              width: '30rem'
+            }}
+          />
+        </Box>
+
+      </Box>
+    </>
+  )
 }
