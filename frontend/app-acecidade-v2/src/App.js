6ebedcb69c9eb68b2b09378b45af03b471000a33
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-02-13 22:43:16 -0300

    mudando a home

--- frontend/app-acecidade-v2/src/App.js
+++ frontend/app-acecidade-v2/src/App.js
@@ -4,7 +4,7 @@ import Contatos from "./page/contatos/Contatos";
 import Home from "./page/home/Home";
 import Locais from "./page/locais/Locais";
 import Menu from "./page/components/menu/menu";
-import Artigos from "./page/artigos/artigos";
+import Acessibilidade from "./page/acessibilidade/Acessibilidade";
 import './index.css'
 import Footer from "./page/components/footer/Footer";
 import VLibras from "@djpfs/react-vlibras";
@@ -18,7 +18,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/contatos" element={<Contatos />}/>
         <Route path="/locais" element={<Locais />}/>
-        <Route path="/Artigos" element={<Artigos />}/>
+        <Route path="/acessibilidade" element={<Acessibilidade />}/>
 
         <Route path="*" element={<h1>Pagina não Encontrada</h1>}/>
         
