0ad2fd34f72d0e8c950c554de0584dab9e728fec
Hélio Carvalho <63817214+heliooak@users.noreply.github.com>
2022-02-15 17:32:42 -0300

    adição de drivers sql

---
+++ backend/api/src/main/java/com/squad26/api/Api1Application.java
@@ -0,0 +1,13 @@
+package com.squad26.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api1Application.class, args);
+	}
+
+}
