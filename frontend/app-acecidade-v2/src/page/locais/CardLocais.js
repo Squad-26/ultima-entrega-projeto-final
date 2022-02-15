eb98fca1cc7fd6593f808cff4c4b7d05b905875a
Renan Terleski <renansktcf@icloud.com>
2022-02-14 22:34:59 -0300

    Locais 14-02

---
+++ frontend/app-acecidade-v2/src/page/locais/CardLocais.js
@@ -0,0 +1,57 @@
+import { Box, Image } from '@skynexui/components'
+import React from 'react'
+import 
+
+function CardRestaurantes({
+  nomeRestaurante = 'trocar nome',
+  descrRest = 'descrição',
+  imgSRC = 'troca imagem',
+  icoDef1='https://images.vexels.com/media/users/3/194664/isolated/lists/f19df9d4bcf1229f9b623358c69db93b-icone-de-alerta-para-deficiencia-visual.png',
+  icoDef2='https://site.sorriso.mt.gov.br/storage/app/uploads/public/a1c/a7d/83e/thumb__600_0_0_0_auto.png',
+  icoDef3 = 'https://imagepng.org/wp-content/uploads/2017/09/simbolo-cadeirante-acessibilidade.png',
+
+}) {
+  return (
+    <div>
+      <Box
+        styleSheet={{
+          backgroundColor: {
+            xl: 'orange'
+          },
+          color: 'white',
+          margin: '16px',
+          padding: '16px',
+          display: 'flex'
+        }}
+      >
+        <Image src={imgSRC} styleSheet={{padding: '16px' }} />
+        <Box styleSheet={{margin: '20px'}}>{nomeRestaurante} </Box>
+        <Box styleSheet={{margin: '20px', width: '50%'}}>{descrRest}</Box>
+        <Box styleSheet= {{height:'50px', width: '50px', margin: '10px'}}></Box>
+        <Box tag='div' styleSheet={{}}>
+          <Image src={icoDef1} styleSheet={{height:'50px', width:'50px', margin: '20px'}}/>
+          <Image src={icoDef2} styleSheet={{height:'50px', width:'50px', margin: '20px'}}/>
+          <Image src={icoDef3} styleSheet={{height:'50px', width:'50px', margin: '20px'}}/>
+        </Box>
+      </Box>
+    </div>
+  )
+}
+
+function Filtro (){
+  return(
+    <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+  </div>
+  )
+}
+
+
+export default (CardRestaurantes, Filtro)
--- frontend/app-acecidade-v2/src/page/locais/Locais.js
+++ frontend/app-acecidade-v2/src/page/locais/Locais.js
@@ -1,90 +1,67 @@
 import React from 'react'
-import { Box, Image } from '@skynexui/components'
+import { Box } from '@skynexui/components'
+import CardRestaurantes from './CardLocais'
+import Filtro from './CardLocais'
+
 export default function Locais() {
   return (
-  <Box styleSheet={{
-    display:'flex'
-  }}>
-    <Box
-  styleSheet={{
-    backgroundColor: 'black',
-    color: 'white',
-    height: '100vh',
-    margin: '35px 10px',
-    padding: '16px',
-    width: '25vw',
-    borderRadius:'10px'
-  }}
->
-  Box Component
-</Box>
-  <Box
-    styleSheet={{
-      backgroundColor: 'black',
-      color: 'white',
-      height: 'auto',
-      margin: '35px 10px',
-      padding: '16px',
-      width: '70vw',
-      borderRadius:'10px'
-    }}
-  >
-
-      <CardRestaurantes nomeRestaurante='bobs' />
-      <CardRestaurantes nomeRestaurante='arroz' />
-      <CardRestaurantes nomeRestaurante='livro' />
-      <CardRestaurantes nomeRestaurante='bobs' />
-      <CardRestaurantes nomeRestaurante='bobs' />
-      <CardRestaurantes nomeRestaurante='bobs' />
-      <CardRestaurantes nomeRestaurante='bobs' />
-      <CardRestaurantes nomeRestaurante='bobs' />
-      <CardRestaurantes nomeRestaurante='bobs' />
-     
-    
-
-  </Box>
-  </Box>
-  )
-}
-
-
-function CardRestaurantes({nomeRestaurante='trocar nome', imgSRC="https://images.vexels.com/media/users/3/128437/isolated/lists/2dd809b7c15968cb7cc577b2cb49c84f-logotipo-de-restaurante-de-comida-de-pizza.png"}){
-  return(
-    <>
-    
-    <Box
+    <Box styleSheet={{ display: 'flex' }}>
+      <Box
         styleSheet={{
-          backgroundColor: {
-            xl: 'orange'
-          },
+          backgroundColor: 'black',
           color: 'white',
-          margin: '16px',
+          height: '100vh',
+          margin: '35px 10px',
           padding: '16px',
-          display: 'flex'
+          width: '25vw',
+          borderRadius: '10px'
         }}
       >
-        <Image
-          src={imgSRC}
-          styleSheet={{
-            border: ''
-          }}
-          />
-          <div>{nomeRestaurante}</div>
+        <Filtro></Filtro>
       </Box>
-    
-    </>
+      <Box
+        styleSheet={{
+          backgroundColor: 'black',
+          color: 'white',
+          height: 'auto',
+          margin: '35px 10px',
+          padding: '16px',
+          width: '70vw',
+          borderRadius: '10px'
+        }}
+      >
+        <CardRestaurantes
+          nomeRestaurante="Museu de Arte de São Paulo Assis Chateaubriand"
+          descrRest ='O Museu de Arte de São Paulo Assis Chateaubriand ou popularmente conhecido como MASP é uma das instituições culturais mais importantes do Brasil.
+          Localizado no coração de São Paulo, é símbolo da capital paulista e referência arquitetônica.
+          Fundado em 1947, o museu possui um acervo de 10 mil obras, porém nem todas já foram expostas no MASP, como uma armadura de cobre e aço de 1480.'
+          imgSRC="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/myczgrokchkt5cb54m0s "
+        />
+
+        <CardRestaurantes
+          nomeRestaurante="Burguer King"
+          descrRest ='Burger King, muitas vezes abreviado como BK, é uma rede de restaurantes especializada em fast-food, fundada nos Estados Unidos por James McLamore e David Edgerton, que abriram a primeira unidade em Miami, Flórida.'
+          imgSRC="https://logospng.org/download/burger-king/logo-burger-king-2021-256.png"
+        />
+        <CardRestaurantes
+          nomeRestaurante="Toca da Cancela"
+          descrRest = 'Restaurante de comida caseira tradicional situado na região central da cidade oferecendo delivery e atendimento no local sempre com comida fresca .'
+          imgSRC="https://tocadacancela.pedzap.com.br/uploads/modules/estabelecimentos/1576_webapp_logomarca_256.png"
+        />
+      
+        <CardRestaurantes
+          nomeRestaurante="Novo Bar"
+          descrRest ='Petiscos e drinks, os melhores chefs e atendimento especial a todos os clientes.'
+          imgSRC="https://tecnologia.systa.com.br/midias/pessoas/24558/20200210162808-guia-estadual--thumb.jpeg"
+        />
+
+        <CardRestaurantes
+          nomeRestaurante="Fit On Me"
+          descrRest ='Saladas, faláfels e muito tempero, sempre balancedo e apetitoso.'
+          imgSRC="https://files.menudino.com/cardapios/18731/logo.png"
+        />
+
+      </Box>
+    </Box>
   )
 }
-
-
-
-/* nessesario em cada req   <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script> */
-
-/* import {GoogleApiWrapper, Map, Maker} from 'google-maps-react'
-
-export const MapContainer = (props) => {}
-
-export default GoogleApiWrapper({
-  apiKay:process.env.GOOGLE_KAY_API,
-  lenguage: 'pt-br'
-})(MapContainer) */
