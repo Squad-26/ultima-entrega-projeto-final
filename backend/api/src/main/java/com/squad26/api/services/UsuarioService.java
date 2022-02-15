78f80ec46aa828d54a01d1412e5ad5f06e32a24b
Hélio Carvalho <63817214+heliooak@users.noreply.github.com>
2022-02-15 13:17:38 -0300

    up

--- backend/api/src/main/java/com/squad26/api/services/UsuarioService.java
+++ backend/api/src/main/java/com/squad26/api/services/UsuarioService.java
@@ -1,8 +1,7 @@
 package com.squad26.api.services;
 
 import java.util.List;
-
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,10 +16,31 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository repository;
 	
+	// GET Method lista geral
 	@Transactional(readOnly = true)
 	public List<Usuario> findAll() {
 		List<Usuario> resultado = repository.findAll();
 		return resultado;
+	}
+	
+	// GET Method por id
+	public Usuario getById(Long id) {
+		Optional<Usuario> usuario = repository.findById(id);
+		if(usuario.isPresent()) {
+			return usuario.get();		
+		} else {
+			return null;
+		}
+	}
+	
+	// POST Method
+	public void save(Usuario usuario) {
+		repository.save(usuario);
+	}
+	
+	// DELETE Method
+	public void delete(Long id) {
+		repository.deleteById(id);
 	}
 		
 }
