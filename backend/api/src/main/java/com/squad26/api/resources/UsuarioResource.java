f75a81d4705068f02de1bf79c812d07c73e3ffe3
Hélio Carvalho <63817214+heliooak@users.noreply.github.com>
2022-02-15 13:24:32 -0300

    inserção de metodos

--- backend/api/src/main/java/com/squad26/api/resources/UsuarioResource.java
+++ backend/api/src/main/java/com/squad26/api/resources/UsuarioResource.java
@@ -3,7 +3,13 @@ package com.squad26.api.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +17,8 @@ import com.squad26.api.entities.Usuario;
 import com.squad26.api.services.UsuarioService;
 
 @RestController
-@RequestMapping(name = "/usuarios")
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "http://localhost:300")
 public class UsuarioResource {
 	
 	@Autowired
@@ -20,5 +27,18 @@ public class UsuarioResource {
 	@GetMapping
 	private List<Usuario> findAll() {
 		return service.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> save(@RequestBody Usuario usuario) {
+		service.save(usuario);
+		return ResponseEntity.ok().body(usuario);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Usuario> delete(@PathVariable Long id) {
+		service.delete(id);
+		Usuario usuario = service.getById(id);
+		return ResponseEntity.ok().body(usuario);
 	}
 }
